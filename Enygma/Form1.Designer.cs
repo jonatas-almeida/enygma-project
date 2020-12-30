8898b8c3beb777996736c4a35face1004c4d8ac2
Jonatas Almeida <jonatas-almeida@users.noreply.github.com>
2020-12-29 23:29:08 -0300

    Commit Inicial - Bug na área de descriptografar o arquivo

---
+++ Enygma/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿
+namespace Enygma
+{
+    partial class Enygma
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnDescriptografar = new System.Windows.Forms.Button();
+            this.btnCreateKeys = new System.Windows.Forms.Label();
+            this.btnImportPublicKey = new System.Windows.Forms.Label();
+            this.btnExportPublicKey = new System.Windows.Forms.Label();
+            this.btnGetPrivateKey = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnCriptografar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.labelMessage = new System.Windows.Forms.Label();
+            this.openFileDialogEncrypt = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialogDecrypt = new System.Windows.Forms.OpenFileDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Montserrat", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(37, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enygma";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(214)))), ((int)(((byte)(117)))));
+            this.panel1.Location = new System.Drawing.Point(43, 122);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(29, 3);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnDescriptografar
+            // 
+            this.btnDescriptografar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(82)))), ((int)(((byte)(82)))), ((int)(((byte)(82)))));
+            this.btnDescriptografar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDescriptografar.FlatAppearance.BorderSize = 0;
+            this.btnDescriptografar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDescriptografar.Font = new System.Drawing.Font("Montserrat", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDescriptografar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnDescriptografar.Location = new System.Drawing.Point(187, 221);
+            this.btnDescriptografar.Name = "btnDescriptografar";
+            this.btnDescriptografar.Size = new System.Drawing.Size(134, 33);
+            this.btnDescriptografar.TabIndex = 3;
+            this.btnDescriptografar.Text = "Descriptografar";
+            this.btnDescriptografar.UseVisualStyleBackColor = false;
+            this.btnDescriptografar.Click += new System.EventHandler(this.btnDescriptografar_Click);
+            // 
+            // btnCreateKeys
+            // 
+            this.btnCreateKeys.AutoSize = true;
+            this.btnCreateKeys.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCreateKeys.Font = new System.Drawing.Font("Montserrat", 8.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCreateKeys.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnCreateKeys.Location = new System.Drawing.Point(38, 23);
+            this.btnCreateKeys.Name = "btnCreateKeys";
+            this.btnCreateKeys.Size = new System.Drawing.Size(80, 15);
+            this.btnCreateKeys.TabIndex = 4;
+            this.btnCreateKeys.Text = "Criar chaves";
+            this.btnCreateKeys.Click += new System.EventHandler(this.btnCreateKeys_Click);
+            // 
+            // btnImportPublicKey
+            // 
+            this.btnImportPublicKey.AutoSize = true;
+            this.btnImportPublicKey.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnImportPublicKey.Font = new System.Drawing.Font("Montserrat", 8.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnImportPublicKey.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnImportPublicKey.Location = new System.Drawing.Point(144, 23);
+            this.btnImportPublicKey.Name = "btnImportPublicKey";
+            this.btnImportPublicKey.Size = new System.Drawing.Size(99, 15);
+            this.btnImportPublicKey.TabIndex = 5;
+            this.btnImportPublicKey.Text = "Importar chave";
+            // 
+            // btnExportPublicKey
+            // 
+            this.btnExportPublicKey.AutoSize = true;
+            this.btnExportPublicKey.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExportPublicKey.Font = new System.Drawing.Font("Montserrat", 8.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExportPublicKey.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnExportPublicKey.Location = new System.Drawing.Point(266, 23);
+            this.btnExportPublicKey.Name = "btnExportPublicKey";
+            this.btnExportPublicKey.Size = new System.Drawing.Size(97, 15);
+            this.btnExportPublicKey.TabIndex = 6;
+            this.btnExportPublicKey.Text = "Exportar chave";
+            // 
+            // btnGetPrivateKey
+            // 
+            this.btnGetPrivateKey.AutoSize = true;
+            this.btnGetPrivateKey.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnGetPrivateKey.Font = new System.Drawing.Font("Montserrat", 8.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGetPrivateKey.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnGetPrivateKey.Location = new System.Drawing.Point(483, 23);
+            this.btnGetPrivateKey.Name = "btnGetPrivateKey";
+            this.btnGetPrivateKey.Size = new System.Drawing.Size(128, 15);
+            this.btnGetPrivateKey.TabIndex = 7;
+            this.btnGetPrivateKey.Text = "Obter chave privada";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(214)))), ((int)(((byte)(117)))));
+            this.panel2.Location = new System.Drawing.Point(79, 122);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(12, 3);
+            this.panel2.TabIndex = 8;
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(82)))), ((int)(((byte)(82)))), ((int)(((byte)(82)))));
+            this.btnSair.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSair.FlatAppearance.BorderSize = 0;
+            this.btnSair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSair.Font = new System.Drawing.Font("Montserrat", 8.999999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSair.Location = new System.Drawing.Point(512, 360);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(99, 29);
+            this.btnSair.TabIndex = 9;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = false;
+            this.btnSair.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnCriptografar
+            // 
+            this.btnCriptografar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(82)))), ((int)(((byte)(82)))), ((int)(((byte)(82)))));
+            this.btnCriptografar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCriptografar.FlatAppearance.BorderSize = 0;
+            this.btnCriptografar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCriptografar.Font = new System.Drawing.Font("Montserrat", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCriptografar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnCriptografar.Location = new System.Drawing.Point(43, 221);
+            this.btnCriptografar.Name = "btnCriptografar";
+            this.btnCriptografar.Size = new System.Drawing.Size(134, 33);
+            this.btnCriptografar.TabIndex = 10;
+            this.btnCriptografar.Text = "Criptografar";
+            this.btnCriptografar.UseVisualStyleBackColor = false;
+            this.btnCriptografar.Click += new System.EventHandler(this.btnCriptografar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Montserrat", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(42, 299);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(223, 18);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "As chaves irão aparecer abaixo";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(214)))), ((int)(((byte)(117)))));
+            this.panel3.Location = new System.Drawing.Point(79, 329);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(12, 3);
+            this.panel3.TabIndex = 13;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(214)))), ((int)(((byte)(117)))));
+            this.panel4.Location = new System.Drawing.Point(43, 329);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(29, 3);
+            this.panel4.TabIndex = 12;
+            // 
+            // labelMessage
+            // 
+            this.labelMessage.AutoSize = true;
+            this.labelMessage.Font = new System.Drawing.Font("Montserrat", 8.999999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMessage.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.labelMessage.Location = new System.Drawing.Point(41, 355);
+            this.labelMessage.Name = "labelMessage";
+            this.labelMessage.Size = new System.Drawing.Size(156, 16);
+            this.labelMessage.TabIndex = 14;
+            this.labelMessage.Text = "Nenhuma chave gerada";
+            // 
+            // openFileDialogEncrypt
+            // 
+            this.openFileDialogEncrypt.FileName = "openFileDialog1";
+            // 
+            // openFileDialogDecrypt
+            // 
+            this.openFileDialogDecrypt.FileName = "openFileDialog1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Montserrat", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(43, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(154, 18);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Extensão do arquivo:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Montserrat", 8.999999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label4.Location = new System.Drawing.Point(43, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(341, 16);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Digite a extensão do arquivo antes de descriptografar";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(204, 167);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(117, 20);
+            this.textBox1.TabIndex = 18;
+            this.textBox1.Text = ".";
+            // 
+            // Enygma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
+            this.ClientSize = new System.Drawing.Size(648, 416);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.labelMessage);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCriptografar);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.btnGetPrivateKey);
+            this.Controls.Add(this.btnExportPublicKey);
+            this.Controls.Add(this.btnImportPublicKey);
+            this.Controls.Add(this.btnCreateKeys);
+            this.Controls.Add(this.btnDescriptografar);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Enygma";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Enygma";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnDescriptografar;
+        private System.Windows.Forms.Label btnCreateKeys;
+        private System.Windows.Forms.Label btnImportPublicKey;
+        private System.Windows.Forms.Label btnExportPublicKey;
+        private System.Windows.Forms.Label btnGetPrivateKey;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnCriptografar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label labelMessage;
+        private System.Windows.Forms.OpenFileDialog openFileDialogEncrypt;
+        private System.Windows.Forms.OpenFileDialog openFileDialogDecrypt;
+        private System.Windows.Forms.TextBox fileExtension;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Enygma/Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Enygma
+{
+
+    public partial class Enygma : Form
+    {
+
+
+        CspParameters cspp = new CspParameters();
+        RSACryptoServiceProvider rsa;
+
+        //Variáveis para o "source", criptografia e descriptografia. Todas devem terminar com contra-barra
+        const string EncrFolder = @"c:\Encrypt\";
+        const string DecrFolder = @"c:\Decrypt\";
+        const string SrcFolder = @"c:\docs\";
+
+        //Arquivo em que a chave publica será gerado
+        const string PubKeyFile = @"c:\encrypt\rsaPublicKey.txt";
+
+        //Chave para fazer a criptografia, depois será implementado um input para o usuário digitar a chave
+        const string keyName = "Key01";
+
+        public Enygma()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //Método para criptografar os arquivos
+        private void btnCriptografar_Click(object sender, EventArgs e)
+        {
+            if (rsa == null)
+            {
+                MessageBox.Show("Chave não definida");
+            }
+            else
+            {
+                //Mostra uma caixa de diálogo para selecionar o arquivo que vai ser criptografado
+                openFileDialogEncrypt.InitialDirectory = SrcFolder;
+                if(openFileDialogEncrypt.ShowDialog() == DialogResult.OK)
+                {
+                    string fileName = openFileDialogEncrypt.FileName;
+                    //Verifica se a variável possui valor
+                    if (fileName != null)
+                    {
+                        FileInfo fileInfo = new FileInfo(fileName);
+                        //Passa o nome do arquivo sem o diretório em que ele se encontra
+                        string name = fileInfo.FullName;
+                        Directory.CreateDirectory(EncrFolder);//Cria o diretório de arquivos Criptografados
+                        EncryptFile(name);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Não foi possível criptografar o arquivo!");
+                    }
+                }
+            }
+        }
+
+        //Método para criptografar o arquivo
+        private void EncryptFile(string inFile)
+        {
+            //Cria a instância do AES para a criptografia simétrica do dado
+            Aes aes = Aes.Create();
+            ICryptoTransform transform = aes.CreateEncryptor();
+
+            //Usa RSACryptoServiceProvider para criptografar a chave AES
+            byte[] keyEncrypted = rsa.Encrypt(aes.Key, false);
+
+            //Cria os arrays para armazenar os valores de tamanho da chave e IV
+            byte[] LenKey = new byte[4];
+            byte[] LenIV = new byte[4];
+
+            int lKey = keyEncrypted.Length;
+            LenKey = BitConverter.GetBytes(lKey);
+            int lIV = aes.IV.Length;
+            LenIV = BitConverter.GetBytes(lIV);
+
+            int startFileName = inFile.LastIndexOf("\\") + 1;
+            //Muda a extensão do arquivo para '.enc'
+            string outFile = EncrFolder + inFile.Substring(startFileName, inFile.LastIndexOf(".") - startFileName) + ".enc";
+
+            using (FileStream outFs = new FileStream(outFile, FileMode.Create))
+            {
+                outFs.Write(LenKey, 0, 4); //Tamanho da chave
+                outFs.Write(LenIV, 0, 4); //Tamanho do IV
+                outFs.Write(keyEncrypted, 0, lKey); //Chave criptografada
+                outFs.Write(aes.IV, 0, lIV); //O conteúdo da cifra
+
+                //Escreve o conteúdo da cifra usando o CryptoStream para criptografar
+                using(CryptoStream outStreamEncrypted = new CryptoStream(outFs, transform, CryptoStreamMode.Write))
+                {
+                    int count = 0;
+                    int offset = 0;
+
+                    int blockSizeBytes = aes.BlockSize / 8;
+                    byte[] data = new byte[blockSizeBytes];
+                    int bytesRead = 0;
+
+                    using (FileStream inFs = new FileStream(inFile, FileMode.Open))
+                    {
+                        do
+                        {
+                            count = inFs.Read(data, 0, blockSizeBytes);
+                            offset += count;
+                            outStreamEncrypted.Write(data, 0, count);
+                            bytesRead += blockSizeBytes;
+                        }
+                        while (count < 0);
+                        inFs.Close();
+                    }
+                    outStreamEncrypted.FlushFinalBlock();
+                    outStreamEncrypted.Close();
+                }
+                outFs.Close();
+            }
+
+        }
+
+        //Método para criar as chaves públicas e privadas utilizando a variável "keyName"
+        private void btnCreateKeys_Click(object sender, EventArgs e)
+        {
+            cspp.KeyContainerName = keyName;
+            rsa = new RSACryptoServiceProvider(cspp);
+            rsa.PersistKeyInCsp = true;
+            if (rsa.PublicOnly == true)
+            {
+                labelMessage.Text = "Key: " + cspp.KeyContainerName + " - Public Only";
+                labelMessage.ForeColor = Color.White;
+            }
+            else
+            {
+                labelMessage.Text = "Key: " + cspp.KeyContainerName + " - Public Only";
+                labelMessage.ForeColor = Color.White;
+            }
+        }
+
+        //Descriptografa um arquivo
+        private void btnDescriptografar_Click(object sender, EventArgs e)
+        {
+            if (rsa == null)
+            {
+                MessageBox.Show("A chave não foi criada");
+            }
+            else
+            {
+                openFileDialogDecrypt.InitialDirectory = EncrFolder;
+                if (openFileDialogDecrypt.ShowDialog() == DialogResult.OK)
+                {
+                    string fileName = openFileDialogEncrypt.FileName;
+                    if(fileName != null)
+                    {
+                        FileInfo fileInfo = new FileInfo(fileName);
+                        string name = fileInfo.Name;
+                        DecryptFile(name);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Não foi possível descriptografar o arquivo");
+                    }
+                }
+            }
+        }
+
+        private void DecryptFile(string inFile)
+        {
+            Aes aes = Aes.Create();
+
+            byte[] LenKey = new byte[4];
+            byte[] LenIV = new byte[4];
+
+
+            string outfile = DecrFolder + inFile.Substring(0, inFile.LastIndexOf(".")) + ".txt";
+
+            using (FileStream inFs = new FileStream(DecrFolder + inFile, FileMode.Open))
+            {
+                inFs.Seek(0, SeekOrigin.Begin);
+                inFs.Seek(0, SeekOrigin.Begin);
+                inFs.Read(LenKey, 0, 3);
+                inFs.Seek(4, SeekOrigin.Begin);
+                inFs.Read(LenIV, 0, 3);
+
+                int lenK = BitConverter.ToInt32(LenKey, 0);
+                int lenIV = BitConverter.ToInt32(LenIV, 0);
+
+                int startC = lenK + lenIV + 8;
+                int lenC = (int)inFs.Length - startC;
+
+                byte[] KeyEcrypted = new byte[lenK];
+                byte[] IV = new byte[lenIV];
+
+                inFs.Seek(8, SeekOrigin.Begin);
+                inFs.Seek(8 + lenK, SeekOrigin.Begin);
+                inFs.Read(KeyEcrypted, 0, lenK);
+                inFs.Read(IV, 0, lenIV);
+
+                byte[] KeyDecrypted = rsa.Decrypt(KeyEcrypted, false);
+
+                ICryptoTransform transform = aes.CreateDecryptor(KeyDecrypted, IV);
+
+                using (FileStream outFs = new FileStream(outfile, FileMode.Create))
+                {
+                    int count = 0;
+                    int offset = 0;
+
+                    int blockSizeBytes = aes.BlockSize / 8;
+                    byte[] data = new byte[blockSizeBytes];
+
+                    inFs.Seek(startC, SeekOrigin.Begin);
+                    using(CryptoStream outStreamDecrypted =  new CryptoStream(outFs, transform, CryptoStreamMode.Write))
+                    {
+                        do
+                        {
+                            count = inFs.Read(data, 0, blockSizeBytes);
+                            offset += count;
+                            outStreamDecrypted.Write(data, 0, count);
+                        }
+                        while (count < 0);
+
+                        outStreamDecrypted.FlushFinalBlock();
+                        outStreamDecrypted.Close();
+                    }
+
+                    outFs.Close();
+                }
+                inFs.Close();
+            }
+        }
+
+        private void fileExtension_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
